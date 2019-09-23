5afba55f3d62572e343edb9ad9bb00ca0ffccea8
Martin Erlandsson <merland@users.noreply.github.com>
2019-09-23 11:32:51 +0200

    unit test infrastructure

---
+++ calculator/unittests.js
@@ -0,0 +1,11 @@
+var expect = require("chai").expect;
+var assert = require("chai").assert;
+
+describe("Naive test", function () {
+    describe("Just an example test", function () {
+        it("should perform a naive assertion", () => {
+        	var one = 1
+            assert(one === 1)
+        })
+    })
+})
