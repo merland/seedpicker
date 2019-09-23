dc5cb05cd4b9a0dd6ad00efcd719e363d863d9c1
Martin Erlandsson <merland@users.noreply.github.com>
2019-09-23 22:28:40 +0200

    some concept code for xpub generation

--- calculator/unittests.js
+++ calculator/unittests.js
@@ -11,17 +11,19 @@ describe("Calculate the 24th word", function () {
     })
 })
 
-describe("XPUB calculation", function () {
+describe("XPUB generation according to https://github.com/iancoleman/bip39/issues/351 (concept code)", function () {
+
     it('can generate a BIP32 xpub', () => {
-        const mnemonic =
-            'empower soul reunion entire help raise truth reflect argue transfer chicken narrow oak friend junior figure auto small push spike next pledge december bridge';
+        const mnemonic = 'empower soul reunion entire help raise truth reflect argue transfer chicken narrow oak friend junior figure auto small push spike next pledge december bridge'
+
         const seed = bip39.mnemonicToSeedSync(mnemonic);
         const node = bip32.fromSeed(seed);
-        const xpub = node.neutered().toBase58();
+        let child = node.derivePath("m/48'/1'/0'/2'")
 
-        assert.strictEqual(
-            xpub,
-            'xpub661MyMwAqRbcEmusjcD3WzYvmgmiK5ttAUy1Pa5pkRCrsPm9cBYoJ9jt6Da9ymucuhHbvdT6kNTKsq6tLMAek2p921jhN6jCFH7AwtJsqXK',
-        );
+        let xpriv = child.toBase58();
+        assert.strictEqual(xpriv, 'xprvA15jYQNsd7SQothyE7MkacnbZv6yiMxeeocf5e9BqCcBfucbSs3jVidbfLC5sbec6swy9uGF6go43hoqZetdnkKixo9d2r6CHM2EWS54qqM');
+
+        const xpub = child.neutered().toBase58();
+        assert.strictEqual(xpub, 'xpub6E55wuumTUzi2NnSL8tkwkjL7wwU7pgW22YFt2YoPY9AYhwjzQMz3Wx5WeReYiPkSwttHVQzLXKgjymgARe7yzpdRgQYFezEqnPSPywibYC');
     });
 })
