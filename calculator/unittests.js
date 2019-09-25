302a7a8f998c8c310d6a02a1d871ac0f7da3f95d
Martin Erlandsson <merland@users.noreply.github.com>
2019-09-25 14:24:57 +0200

    added a commented unit test for mainnet Zpub

--- calculator/unittests.js
+++ calculator/unittests.js
@@ -28,4 +28,14 @@ describe("XPUB generation according to https://github.com/iancoleman/bip39/issue
         assert.strictEqual(result.xpub, 'xpub6E55wuumTUzi2NnSL8tkwkjL7wwU7pgW22YFt2YoPY9AYhwjzQMz3Wx5WeReYiPkSwttHVQzLXKgjymgARe7yzpdRgQYFezEqnPSPywibYC');
         //TODO// assert.strictEqual(result.zpub, 'Zpub74dhgUz3Koe39YL3wWvzC1G9BgGdDRM6AXENN5c7XKjLH697G85PNm8HMmJJ6xvVVhCUf2cE94RHdicZu3d6ixJVzpunpt6DGxnbm6xG5Ue');
     });
+
+
+    it('can generate a Zpub -  bitcoin mainnet P2WSH', () => {
+        const mnemonic = 'empower soul reunion entire help raise truth reflect argue transfer chicken narrow oak friend junior figure auto small push spike next pledge december bridge'
+        let derivationPath = "m/48'/0'/0'/2'";
+        const result = seedpicker.xpubFromMnemonic(mnemonic, derivationPath);
+        assert.strictEqual(result.xpub, 'xpub6Do9J2BK1ZPtKkc71z1ohMH2N26eRa6yZu65hEijtk1F8LX3yjdNWTBv6p1yVgqE3RnVoesvFDnbTYkD1rbJo3opse5P75jELRJcR9ev9rU');
+        //TODO assert.strictEqual(result.zpub, 'Zpub74Mm2bFast3DSv9idN42wboqRkRoXAmZiPnCBHn42XbQriiRFTLmqhN7wvtd3wMy6B66BC5A3ktCMHb6kUaHY1HhSnadgJqCmbhmnAQispN');
+    });
+
 })
