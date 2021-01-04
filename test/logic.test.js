0a743f5a701fa8eab29d2f24ba938fd4f995ea61
Jimbo Wilson <wilson.jim.r@gmail.com>
2021-01-04 09:52:14 -0500

    Implementing optional 24th word input for additional entropy.

--- test/logic.test.js
+++ test/logic.test.js
@@ -20,7 +20,12 @@ describe("Calculate the 24th word", function () {
 
     //https://github.com/merland/seedpicker/issues/13
     it("should select the first checksum word, alphabetically", () => {
-        const my23words = "empower soul reunion entire help raise truth reflect argue transfer chicken narrow oak friend junior figure auto small push spike next pledge december"
+        const my23words = [
+          "empower", "soul", "reunion", "entire", "help", "raise", "truth",
+          "reflect", "argue", "transfer", "chicken", "narrow", "oak", "friend",
+          "junior", "figure", "auto", "small", "push", "spike", "next",
+          "pledge", "december"
+        ]
         const lastWord = logic.firstChecksumWordAlphabetically(my23words);
 
         const expectedWords = ["bridge"]
